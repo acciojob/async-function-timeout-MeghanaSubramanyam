1fc918358a9480dd58848a9f98d6d5877a0ac42b
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-10-20 18:11:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let text=document.getElementById("text");
+let del=document.getElementById("delay");
+let di=document.getElementById("output");
+
+async function delay(){
+	return new Promise((res)=>{
+		setTimeout(()=>{
+			res(di.textContent(text));
+		})
+	})
+	
+}
